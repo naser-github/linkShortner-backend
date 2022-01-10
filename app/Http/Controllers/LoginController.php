07ef9803dd0252af9f57110c06a5351359ad3df7
tayson13 <ab.naser01@gmail.com>
2022-01-11 04:50:41 +0600

    login on process

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,14 +10,12 @@ class LoginController extends Controller
 {
     function login(Request $request)
     {
-
         $user = User::where('email', $request->email)->first();
 
-        // print_r($data);
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response([
                 'message' => ['These credentials do not match our records.']
-            ], 404);
+            ], 401);
         }
 
         $token = $user->createToken('my-app-token')->plainTextToken;
